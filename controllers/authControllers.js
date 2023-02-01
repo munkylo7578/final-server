782dfda1c25ad1d267cb87041d2e76ecb37a7e4c
munkylo7578 <donam2832000@gmail.com>
2023-02-01 08:33:49 +0700

    first commit

---
+++ controllers/authControllers.js
@@ -0,0 +1,34 @@
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+
+exports.createOrUpdateUser = asyncHandler(async (req, res) => {
+  const { name, picture, email } = req.user
+  console.log(req.user,"6666")
+
+  const user = await User.findOneAndUpdate(
+    { email },
+    { name, picture },
+    { new: true }
+  )
+  if (user) {
+    res.json(user)
+  } else {
+    const newUser = await new User({
+      email,
+      name,
+      picture,
+    }).save()
+
+    res.json(newUser)
+  }
+})
+
+exports.currentUser = asyncHandler(async (req, res) => {
+  const user = await User.findOne({ email: req.user.email })
+  if (user) {
+    res.status(201).json(user)
+  } else {
+    res.status(401)
+    throw new Error('User Not Found')
+  }
+})
